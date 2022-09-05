99bbeb16cd959abdfa0786224944da14715eeec1
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-05 15:43:38 -0500

    RequestPara con multiples parametros.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/main/java/com/fmontalvoo/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/fmontalvoo/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.fmontalvoo.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +21,21 @@ public class EjemploParamsController {
 	@GetMapping("/string")
 	public String param(@RequestParam(name = "texto", required = false, defaultValue = "...") String parametro,
 			Model model) {
-		model.addAttribute("parametro", "El parametro recibido es: " + parametro);
+		model.addAttribute("res", "El parametro recibido es: " + parametro);
+		return "params/view";
+	}
+
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String texto, @RequestParam Integer numero, Model model) {
+		model.addAttribute("res", "Texto: " + texto + "; Numero: " + numero);
+		return "params/view";
+	}
+
+	@GetMapping("/mix-params-req")
+	public String param(HttpServletRequest request, Model model) {
+		String texto = request.getParameter("texto");
+		Integer numero = Integer.parseInt(request.getParameter("numero"));
+		model.addAttribute("res", "Texto: " + texto + "; Numero: " + numero);
 		return "params/view";
 	}
 
